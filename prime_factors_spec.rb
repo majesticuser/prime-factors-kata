359c58af9042b31058e1fbe269eced1be4c2f547
Virginio Carfagno <virginio.carfagno@mindmatters.de>
2012-04-27 16:29:48 +0200

    Added factor_into matcher

--- prime_factors_spec.rb
+++ prime_factors_spec.rb
@@ -2,48 +2,37 @@ require File.dirname(__FILE__) + '/prime_factors'
 
 describe "PrimeFactors#get" do
 
-  it 'returns [] for 0 ' do
-    PrimeFactors.get(0).should eq([])
+  it {0.should factor_into([])}
+
+  it {1.should factor_into([])}
+
+  it {2.should factor_into([2])}
+
+  it {3.should factor_into([3])}
+
+  it {4.should factor_into([2,2])}
+
+  it {5.should factor_into([5])}
+
+  it {6.should factor_into([2, 3])}
+
+  it {7.should factor_into([7])}
+
+  it {8.should factor_into([2,2,2])}
+
+  it {9.should factor_into([3,3])}
+
+  it {16.should factor_into([2,2,2,2])}
+
+
+  RSpec::Matchers.define :factor_into do |expected|
+    match do |actual|
+      PrimeFactors.get(actual) == expected
+    end
+
+    failure_message_for_should do |actual|
+      "expected #{actual} to factor into #{expected} but was #{PrimeFactors.get(actual)}"
+    end
   end
-
-  it 'returns [] for 1' do
-    PrimeFactors.get(1).should eq([])
-  end
-
-  it 'returns [2] for 2' do
-    PrimeFactors.get(2).should eq([2])
-  end
-
-  it 'returns [3] for 3' do
-    PrimeFactors.get(3).should eq([3])
-  end
-
-  it 'returns [2, 2] for 4' do
-    PrimeFactors.get(4).should eq([2, 2])
-  end
-
-  #it 'returns [5] for 5' do
-  #  PrimeFactors.get(5).should eq([5])
-  #end
-
-  it 'returns [2, 3] for 6' do
-    PrimeFactors.get(6).should eq([2, 3])
-  end
-
-  #it 'returns [7] for 7' do
-  #  PrimeFactors.get(7).should eq([7])
-  #end
-
-  it 'returns [2, 2, 2] for 8' do
-    PrimeFactors.get(8).should eq([2, 2, 2])
-  end
-
-  it 'returns [3, 3] for 9' do
-    PrimeFactors.get(9).should eq([3, 3])
-  end
-
-  #it 'example' do
-  #  puts PrimeFactors.get(827289).inspect
-  #end
 
 end
